f616548fb8d987067b8087abccdc5c84ae82d39e
SONAL <sonalnpande@gmail.com>
2018-06-16 17:21:18 +0530

    angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule,
